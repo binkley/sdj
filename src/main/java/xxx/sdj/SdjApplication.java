4ba2d014e19e13e2e7ad47f91dce8a1656dd64eb
Brian Oxley <boxley@thoughtworks.com>
2018-02-14 05:22:13 -0800

    Initial build from Spring Initialzr

---
+++ src/main/java/xxx/sdj/SdjApplication.java
@@ -0,0 +1,12 @@
+package xxx.sdj;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class SdjApplication {
+    public static void main(final String... args) {
+        run(SdjApplication.class, args);
+    }
+}
---
+++ src/test/java/xxx/sdj/SdjApplicationTests.java
@@ -0,0 +1,14 @@
+package xxx.sdj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SdjApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
