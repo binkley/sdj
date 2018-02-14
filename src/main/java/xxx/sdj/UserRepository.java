6e4ff720c5bb90d57cb59291ed9c90f105afb986
Brian Oxley <boxley@thoughtworks.com>
2018-02-14 05:36:42 -0800

    It runs

---
+++ src/main/java/xxx/sdj/UserRepository.java
@@ -0,0 +1,6 @@
+package xxx.sdj;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UserRepository
+        extends PagingAndSortingRepository<User, Long> {}
