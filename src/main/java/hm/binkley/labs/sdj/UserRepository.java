64b6b17e19d7cac2a64f10642d377c943e9b3f88
Brian Oxley <boxley@thoughtworks.com>
2018-02-14 06:51:35 -0800

    Simpler JPA

--- src/main/java/hm/binkley/labs/sdj/UserRepository.java
+++ src/main/java/hm/binkley/labs/sdj/UserRepository.java
@@ -1,11 +1,10 @@
 package hm.binkley.labs.sdj;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRepository
         extends PagingAndSortingRepository<User, Long> {
-    List<User> findUserByNickName(@Param("nickName") final String nickName);
+    List<User> findByNickName(final String nickName);
 }
