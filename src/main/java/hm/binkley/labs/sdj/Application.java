e483bbc23c9f05b22058202a094d8d4dd03a27cd
Brian Oxley <boxley@thoughtworks.com>
2018-02-14 06:39:55 -0800

    Let Swagger build my UI

--- src/main/java/hm/binkley/labs/sdj/Application.java
+++ src/main/java/hm/binkley/labs/sdj/Application.java
@@ -1,10 +1,16 @@
 package hm.binkley.labs.sdj;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration
+        .SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@EnableSwagger2
+@Import(SpringDataRestConfiguration.class)
 public class Application {
     public static void main(final String... args) {
         run(Application.class, args);
